d62b8caa128d2a40b1a0bd493847fbeecc120299
Rupam Kundu <rupamkundu1000000@gmail.com>
2023-05-13 16:21:00 +0530

    Update constants.ts
    Updated constants.ts with the AWS Elastic Beanstalk URL.

--- client/src/types/constants.ts
+++ client/src/types/constants.ts
@@ -1,1 +1,2 @@
-export const SERVER_ADDRESS = "http://localhost:5000";
+// export const SERVER_ADDRESS = "http://localhost:5000";
+export const SERVER_ADDRESS = "http://q-and-a-bot-env.ap-south-1.elasticbeanstalk.com/";
