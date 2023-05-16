621082a43de427c5d0c75483c540c0c11e387243
Rupam Kundu <rupamkundu1000000@gmail.com>
2023-05-16 15:18:11 +0530

    Reset application configuration

--- client/src/types/constants.ts
+++ client/src/types/constants.ts
@@ -1,2 +1,1 @@
-// export const SERVER_ADDRESS = "http://localhost:5000";
-export const SERVER_ADDRESS = "http://q-and-a-bot-env.ap-south-1.elasticbeanstalk.com/";
+export const SERVER_ADDRESS = "http://localhost:5000";
